75dea57fd24d44f59a72159848cdaddb34540a6a
JinweiOS <jinwei.peng@beingthink.com>
2023-11-05 16:12:03 +0800

    feat: 后端代码

--- pages/mysql/mysql.js
+++ pages/mysql/mysql.js
@@ -1,5 +1,16 @@
 // pages/mysql/mysql.js
 Page({
+  onLoad() {
+    const configStr = wx.getStorageSync('mysqlinfo')
+    const config = JSON.parse(configStr)
+    this.setData({
+      'formData.dbname': config.dbname,
+      'formData.dbip': config.dbip,
+      'formData.dbport': config.dbport,
+      'formData.username': config.username,
+      'formData.password': config.password
+    })
+  },
   data: {
     formData: {
       dbname: '',
@@ -9,11 +20,6 @@ Page({
       password: ''
     },
     agree: false,
-  },
-  onLoad() {
-    this.setData({
-      [`formData.password`]: '1'
-    })
   },
   formInputChange(event) {
     const inputType = event.currentTarget.dataset.key
@@ -29,29 +35,33 @@ Page({
   },
   requestMysql() {
     // TODO1: 校验agree 
-
     if (!this.data.agree) {
       wx.showToast({
         title: '请先同意声明',
         icon: 'error'
       })
+      return;
     }
     // TODO2: 请求后端接口，完成账号申请
-    const result = {
-      dbname: 'tuchuang_test',
-      dbip: '192.168.151.131',
-      dbport: '3306',
-      username: 'tuchuang',
-      password: '!QAZ2wsx'
-    }
-    console.log('触发')
-
-    this.setData({
-      'formData.dbname': result.dbname,
-      'formData.dbip': result.dbip,
-      'formData.dbport': result.dbport,
-      'formData.username': result.username,
-      'formData.password': result.password
+    wx.request({
+      url: 'http://10.3.9.27:3000/mysql/info',
+      success: (data) => {
+        console.log(data.data.data)
+        const result = data.data.data
+        wx.setStorageSync('mysqlinfo', JSON.stringify(data.data.data))
+        this.setData({
+          'formData.dbname': result.dbname,
+          'formData.dbip': result.dbip,
+          'formData.dbport': result.dbport,
+          'formData.username': result.username,
+          'formData.password': result.password
+        })
+      }
+    })
+  },
+  copyConfig() {
+    wx.setClipboardData({
+      data: JSON.stringify(this.data.formData),
     })
   }
 })
