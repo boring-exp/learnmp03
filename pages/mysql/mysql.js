0bad180df87d996801b93460b1247d25c88dfc4b
JinweiOS <jinwei.peng@beingthink.com>
2023-11-05 10:59:44 +0800

    feat: 完成小程序申请页面

--- pages/mysql/mysql.js
+++ pages/mysql/mysql.js
@@ -2,17 +2,56 @@
 Page({
   data: {
     formData: {
-      dbname: '1',
-      dbip: '2',
-      dbport: '3',
-      username: '4',
-      password: '5'
-    }
+      dbname: '',
+      dbip: '',
+      dbport: '',
+      username: '',
+      password: ''
+    },
+    agree: false,
   },
   onLoad() {
     this.setData({
       [`formData.password`]: '1'
     })
   },
-  formInputChange() {}
+  formInputChange(event) {
+    const inputType = event.currentTarget.dataset.key
+    this.setData({
+      [`formData.${inputType}`]: event.detail.value
+    })
+  },
+  bindAgreeChange(e) {
+    const agree = e.detail.value.length > 0
+    this.setData({
+      agree: agree
+    })
+  },
+  requestMysql() {
+    // TODO1: 校验agree 
+
+    if (!this.data.agree) {
+      wx.showToast({
+        title: '请先同意声明',
+        icon: 'error'
+      })
+    }
+    // TODO2: 请求后端接口，完成账号申请
+    const result = {
+      dbname: 'tuchuang_test',
+      dbip: '192.168.151.131',
+      dbport: '3306',
+      username: 'tuchuang',
+      password: '!QAZ2wsx'
+    }
+    console.log('触发')
+
+    this.setData({
+      'formData.dbname': result.dbname,
+      'formData.dbip': result.dbip,
+      'formData.dbport': result.dbport,
+      'formData.username': result.username,
+      'formData.password': result.password
+    })
+  }
 })
